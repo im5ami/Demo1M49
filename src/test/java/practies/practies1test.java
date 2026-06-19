545aca225924c7bc96388a79186ccc70eba1b02e
Admin <Admin@LAPTOP-0F5NG102>
2026-06-19 11:07:24 +0530

    comited

---
+++ src/test/java/practies/practies1test.java
@@ -0,0 +1,12 @@
+package practies;
+
+import org.testng.annotations.Test;
+
+public class practies1test 
+{
+	@Test(groups="regression")
+	public void food2test()
+	{
+		System.out.println("rice");
+	}
+}
---
+++ src/test/java/practies/practies2test.java
@@ -0,0 +1,12 @@
+package practies;
+
+import org.testng.annotations.Test;
+
+public class practies2test 
+{
+	@Test(groups="smoke")
+	public void food1test()
+	{
+		System.out.println("vada pav");
+	}
+}
